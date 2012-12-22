2f3d3b100944e3c1d9bb930289753700a070fae6
Logs <logs.be@gmail.com>
2012-12-22 21:05:25 +0100

    Ticket #18 : Sélectionner les phrases pour le backward speaking Nicobot ne dois pas retourner les urls ni les messages trop longs.

--- src/main/java/com/st/nicobot/behavior/BackwardsSpeaking.java
+++ src/main/java/com/st/nicobot/behavior/BackwardsSpeaking.java
@@ -1,5 +1,7 @@
 package com.st.nicobot.behavior;
 
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.CharUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -12,24 +14,47 @@ import com.st.nicobot.utils.Option;
  */
 public class BackwardsSpeaking implements NiConduct {
 
+	/**
+	 * Nombre maximum de char que la phrase doit contenir pour être inversée
+	 */
+	private static int MAX_LENGTH_TO_REVERSE = 50;
+	private static Pattern pattern = Pattern.compile("(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?");
+	
 	@Override
 	public int getChance() {
-		return 50;
+		return 500;
 	}
 
 	@Override
 	public void behave(NicoBot nicobot, Option opts) {
-		nicobot.sendMessage(opts.channel, reverseString(opts.message));
+		final String reversedMessage = reverseString(opts.message);
+		if(reversedMessage != null) {
+			nicobot.sendMessage(opts.channel, reversedMessage);
+		}
 	}
 	
 	/**
 	 * Retourne {@code string} renversé. <br/>
 	 * Si une majuscule est présente sur le 1er caractere de {@code string}, 
-	 * alors le 1er car de la chaine renversée sera en maj aussi.
+	 * alors le 1er char de la chaine renversée sera en maj aussi.
+	 * Conditions d'annulation :
+	 * - La phrase contient ou est une url;
+	 * - La phrase dépasse {@link MAX_LENGTH_TO_REVERSE} caractères
 	 * @param string
 	 * @return
 	 */
 	protected String reverseString(String string) {
+		
+		// 1. Si c'est une url, on passe 
+		if(pattern.matcher(string).find()) {
+			return null;
+		}
+		
+		// 2. Si la phrase est trop longue, on passe aussi
+		if(string.length() > MAX_LENGTH_TO_REVERSE) {
+			return null;
+		}
+		
 		boolean isFirstLetterUpperCase = CharUtils.isAsciiAlphaUpper(string.charAt(0));
 
 		StringBuilder builder = new StringBuilder(string).reverse();
